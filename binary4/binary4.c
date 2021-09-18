fed8bdcf0775dde7179415d0bb67638dfc2cb7ac
root <zhangqianyu.sys@bytedance.com>
2021-09-18 20:29:34 +0800

    add binary4

---
+++ binary4/binary4.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+
+#include <repo4/library4/public/library4.h>
+// #include <repo2/library2/public/library2.h>
+// #include <repo1/library1/public/library1.h>
+
+
+int main()
+{
+    // repo1_library1_display();
+    // repo2_library2_display();
+    repo4_library4_display();
+
+    return 0;
+}
